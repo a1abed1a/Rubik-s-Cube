20ac1524508e4b5346e46b3341fff61b4f130380
a1abed1a <>
2021-07-15 13:37:07 +0000

    Added a css code

---
+++ app.js
@@ -0,0 +1,23 @@
+var name = prompt("What should i call you.")
+var answer = prompt("Welcome "+name+", what stage do you want , if you entered a stage that not available, it will show all the stages.")
+if (answer == 1){
+  document.write("<div><h4>Stage 1</h4><img src='https://i.pinimg.com/564x/ab/3b/13/ab3b1391ddfd0fa6e3cf8ed7b3916167.jpg' alt='Stage 1' width='468' height='655'> </div>")
+}
+  else if (answer == 2){
+  document.write("<div><h4>Stage 2</h4><img src='https://i.pinimg.com/564x/3b/9b/cd/3b9bcd4db8847787fada2565ef63ce04.jpg' alt='Stage 2' width='468' height='655'> </div>")
+  }
+  else if (answer == 3){
+  document.write("<div><h4>Stage 3</h4><img src='https://i.pinimg.com/564x/b3/1f/99/b31f99a63cc84b752890da02d688ec90.jpg' alt='Stage 3' width='468' height='655'> </div>")
+  }
+  else if (answer == 4){
+  document.write("<div><h4>Stage 4</h4><img src='https://i.pinimg.com/564x/05/7b/75/057b753b20dd51acb9d986d3cd8d9593.jpg' alt='Stage 4' width='468' height='655'> </div>")
+  }
+  else if (answer == 5){
+  document.write("<div><h4>Stage 5</h4><img src='https://i.pinimg.com/564x/c0/1c/7a/c01c7a25b0ef13b4df7fcb7e360aa0fa.jpg'  alt='Stage 5' width='468' height='655'> </div>")
+  }
+  else if (answer == 6){
+  document.write("<div><h4>Stage 6</h4><img src='https://i.pinimg.com/564x/6d/ec/43/6dec433d7afe9c75c764ab63aae733d3.jpg' alt='Stage 6' width='468' height='655'> </div>")
+  }
+  else {
+  document.write("<div><h4>Stage 1</h4><img src='https://i.pinimg.com/564x/ab/3b/13/ab3b1391ddfd0fa6e3cf8ed7b3916167.jpg' alt='Stage 1' width='468' height='655'> </div> <div><h4>Stage 2</h4><img src='https://i.pinimg.com/564x/3b/9b/cd/3b9bcd4db8847787fada2565ef63ce04.jpg' alt='Stage 2' width='468' height='655'> </div> <div><h4>Stage 3</h4><img src='https://i.pinimg.com/564x/b3/1f/99/b31f99a63cc84b752890da02d688ec90.jpg' alt='Stage 3' width='468' height='655'> </div> <div><h4>Stage 4</h4><img src='https://i.pinimg.com/564x/05/7b/75/057b753b20dd51acb9d986d3cd8d9593.jpg' alt='Stage 4' width='468' height='655'> </div> <div><h4>Stage 5</h4><img src='https://i.pinimg.com/564x/c0/1c/7a/c01c7a25b0ef13b4df7fcb7e360aa0fa.jpg'  alt='Stage 5' width='468' height='655'> </div> <div><h4>Stage 6</h4><img src='https://i.pinimg.com/564x/6d/ec/43/6dec433d7afe9c75c764ab63aae733d3.jpg' alt='Stage 6' width='468' height='655'> </div>")
+  }
